7bf159833f673a17b0cd3a884c4ad9d1f0dbaf74
maimu <m0smile10often@gmail.com>
2023-09-13 23:28:38 +0900

    エラーメッセージを改良

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -22,12 +22,28 @@ struct Token
   char *str;
 };
 
+char *user_input;
+
 Token *token;
 
 void error(char *fmt, ...)
 {
   va_list ap;
   va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+void error_at(char *loc, char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, "");
+  fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
