e3966b0d995c129ddbf13c68f585736320b217d7
maimu <m0smile10often@gmail.com>
2023-09-17 23:13:57 +0900

    四則演算のできる言語の作成

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -5,27 +5,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+//
+// Tokenizer
+//
+
 typedef enum
 {
-  TK_RESERVED,
-  TK_NUM,
-  TK_EOF,
+  TK_RESERVED, // Keywords or punctuators
+  TK_NUM,      // Integer literals
+  TK_EOF,      // End-of-file markers
 } TokenKind;
 
+// Token type
 typedef struct Token Token;
-
 struct Token
 {
-  TokenKind kind;
-  Token *next;
-  int val;
-  char *str;
+  TokenKind kind; // Token kind
+  Token *next;    // Next token
+  int val;        // If kind is TK_NUM, its value
+  char *str;      // Token string
 };
 
+// Input program
 char *user_input;
 
+// Current token
 Token *token;
 
+// Reports an error and exit.
 void error(char *fmt, ...)
 {
   va_list ap;
@@ -35,6 +42,7 @@ void error(char *fmt, ...)
   exit(1);
 }
 
+// Reports an error location and exit.
 void error_at(char *loc, char *fmt, ...)
 {
   va_list ap;
@@ -42,24 +50,35 @@ void error_at(char *loc, char *fmt, ...)
 
   int pos = loc - user_input;
   fprintf(stderr, "%s\n", user_input);
-  fprintf(stderr, "%*s", pos, "");
+  fprintf(stderr, "%*s", pos, ""); // print pos spaces.
   fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
 }
 
+// Consumes the current token if it matches `op`.
 bool consume(char op)
 {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("'%c'ではありません", op);
+    return false;
+  token = token->next;
+  return true;
+}
+
+// Ensure that the current token is `op`.
+void expect(char op)
+{
+  if (token->kind != TK_RESERVED || token->str[0] != op)
+    error_at(token->str, "expected '%c'", op);
   token = token->next;
 }
 
+// Ensure that the current token is TK_NUM.
 int expect_number()
 {
   if (token->kind != TK_NUM)
-    error("数ではありません");
+    error_at(token->str, "expected a number");
   int val = token->val;
   token = token->next;
   return val;
@@ -70,6 +89,7 @@ bool at_eof()
   return token->kind == TK_EOF;
 }
 
+// Create a new token and add it as the next token of `cur`.
 Token *new_token(TokenKind kind, Token *cur, char *str)
 {
   Token *tok = calloc(1, sizeof(Token));
@@ -79,26 +99,31 @@ Token *new_token(TokenKind kind, Token *cur, char *str)
   return tok;
 }
 
-Token *tokenize(char *p)
+// Tokenize `user_input` and returns new tokens.
+Token *tokenize()
 {
+  char *p = user_input;
   Token head;
   head.next = NULL;
   Token *cur = &head;
 
   while (*p)
   {
+    // Skip whitespace characters.
     if (isspace(*p))
     {
       p++;
       continue;
     }
 
-    if (*p == '+' || *p == '-')
+    // Punctuator
+    if (strchr("+-*/()", *p))
     {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
 
+    // Integer literal
     if (isdigit(*p))
     {
       cur = new_token(TK_NUM, cur, p);
@@ -106,42 +131,166 @@ Token *tokenize(char *p)
       continue;
     }
 
-    error("トークナイズできません");
+    error_at(p, "invalid token");
   }
 
   new_token(TK_EOF, cur, p);
   return head.next;
 }
 
+//
+// Parser
+//
+
+typedef enum
+{
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // Integer
+} NodeKind;
+
+// AST node type
+typedef struct Node Node;
+struct Node
+{
+  NodeKind kind; // Node kind
+  Node *lhs;     // Left-hand side
+  Node *rhs;     // Right-hand side
+  int val;       // Used if kind == ND_NUM
+};
+
+Node *new_node(NodeKind kind)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs)
+{
+  Node *node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_num(int val)
+{
+  Node *node = new_node(ND_NUM);
+  node->val = val;
+  return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+// expr = mul ("+" mul | "-" mul)*
+Node *expr()
+{
+  Node *node = mul();
+
+  for (;;)
+  {
+    if (consume('+'))
+      node = new_binary(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_binary(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+// mul = primary ("*" primary | "/" primary)*
+Node *mul()
+{
+  Node *node = primary();
+
+  for (;;)
+  {
+    if (consume('*'))
+      node = new_binary(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_binary(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+// primary = "(" expr ")" | num
+Node *primary()
+{
+  if (consume('('))
+  {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  return new_num(expect_number());
+}
+
+//
+// Code generator
+//
+
+void gen(Node *node)
+{
+  if (node->kind == ND_NUM)
+  {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind)
+  {
+  case ND_ADD:
+    printf("  add rax, rdi\n");
+    break;
+  case ND_SUB:
+    printf("  sub rax, rdi\n");
+    break;
+  case ND_MUL:
+    printf("  imul rax, rdi\n");
+    break;
+  case ND_DIV:
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+    break;
+  }
+
+  printf("  push rax\n");
+}
+
 int main(int argc, char **argv)
 {
   if (argc != 2)
-  {
+    error("%s: invalid number of arguments", argv[0]);
 
-    error("引数の個数が正しくありません");
-    return 1;
-  }
+  // Tokenize and parse.
+  user_input = argv[1];
+  token = tokenize();
+  Node *node = expr();
 
-  token = tokenize(argv[1]);
-
+  // Print out the first half of assembly.
   printf(".intel_syntax noprefix\n");
-  printf(".globl main\n");
+  printf(".global main\n");
   printf("main:\n");
 
-  printf("  mov rax, %d\n", expect_number());
+  // Traverse the AST to emit assembly.
+  gen(node);
 
-  while (!at_eof())
-  {
-    if (consume('+'))
-    {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  // A result must be at the top of the stack, so pop it
+  // to RAX to make it a program exit code.
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
